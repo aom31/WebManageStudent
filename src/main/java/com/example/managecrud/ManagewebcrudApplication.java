5c25528ecf251c57f0f1d896e31657a98fdf6164
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-25 19:12:49 +0700

    create

---
+++ src/main/java/com/example/managecrud/ManagewebcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.managecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagewebcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagewebcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managecrud/ManagewebcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagewebcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
